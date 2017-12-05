24d4060bf3caf3cf75f3277fabc2907ecd1eac95
cryszero <C:\Users\\AppData\Roaming\The Bat!>
2017-12-05 18:36:05 +0500

    Правки под ие, переделал JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
-document.getElementById("search-form").style.display="none";
-var visible = false;
-function close_open_form() {
-  if(visible) {
-    document.getElementById("search-form").style.display="none";
-    visible = false;
+var form = document.querySelector(".search-form");
+var link = document.querySelector(".form-call");
+var arrival = document.getElementById("arrival-date");
+var departure = document.getElementById("departure-date");
+var adults = document.getElementById("adults-input");
+var kids = document.getElementById("kids-input");
+
+link.addEventListener("click", function(evt) {
+  form.classList.toggle("form-animation");
+  form.classList.toggle("form-hide");
+  form.classList.remove("form-error");
+});
+
+document.addEventListener("DOMContentLoaded", function(evt) {
+  document.querySelector(".search-form").classList.add("form-hide");
+});
+
+form.addEventListener("submit", function(evt) {
+  if(!arrival.value || !departure.value || !adults.value || !kids.value) {
+    evt.preventDefault();
+    form.classList.remove("form-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("form-error");
   }
   else {
-    document.getElementById("search-form").style.display="flex";
-    visible = true;
+    form.classList.remove("form-error");
   }
-}
+});
 
 function initMap() {
   var uluru = {lat: 34.869, lng: -111.760};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: {lat: 34.706088, lng: -111.781723}
   });
